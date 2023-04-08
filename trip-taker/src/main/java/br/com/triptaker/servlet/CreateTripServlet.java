3834f32b3da4cab9cd146b6e68a1590578497cb5
Vinicius Garcia <vinicius_souzagarcia@hotmail.com>
2023-04-08 15:59:32 -0300

    Insert Create/Edit Trip

--- trip-taker/src/main/java/br/com/triptaker/servlet/CreateTripServlet.java
+++ trip-taker/src/main/java/br/com/triptaker/servlet/CreateTripServlet.java
@@ -9,17 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
-@WebServlet("/create-car")
+@WebServlet("/create-trip")
 public class CreateTripServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String tripName = request.getParameter("car-name");
+        String local = request.getParameter("local");
+        int qtdPessoa = Integer.parseInt(request.getParameter("qtdPessoa"));
+        String guiaResponsavel = request.getParameter("guiaResponsavel");
+        Double vlrUnitario = Double.parseDouble(request.getParameter("vlrUnitario"));
+        String dtInicio = request.getParameter("dtInicio");
+        String dtFinal = request.getParameter("dtFinal");
+        String descricao = request.getParameter("descricao");
 
-        System.out.println(tripName);
 
         createEditTrip createEditTrip = new createEditTrip();
-        createEditTrip.setLocal(tripName);
+        createEditTrip.setLocal(local);
+        createEditTrip.setQtdPessoas(qtdPessoa);
+        createEditTrip.setGuiaResponsavel(guiaResponsavel);
+        createEditTrip.setValorUnitario(vlrUnitario);
+        createEditTrip.setDataInicio(dtInicio);
+        createEditTrip.setDataFinal(dtFinal);
+        createEditTrip.setDescricao(descricao);
+
+
         new TripTakerDAO().createTrip(createEditTrip);
         request.getRequestDispatcher("index.html").forward(request, response);
     }
